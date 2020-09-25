8422d5a8a0caf04638cf2efdfbd1a11a9e9d9d79
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-25 04:03:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Ken"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Florida, P.R"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is by Rihanna"
+  end 
 end
